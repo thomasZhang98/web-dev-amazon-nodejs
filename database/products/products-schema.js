cb51633fef9e6614dbef54dfcacf64702bd64053
Aaron Master <aaron@Aarons-MacBook-Pro.local>
2022-04-20 12:23:37 -0400

    split ID fields and added bookmarks to schema

--- database/products/products-schema.js
+++ database/products/products-schema.js
@@ -1,14 +1,16 @@
 const mongoose = require("mongoose");
 const productsSchema = mongoose.Schema(
   {
-    pid: String,
-    title: String,
+    productID: String,  // ID for the whole product, get from amazonAPI 'asin' field
+    itemID: String, // ID for individual item, generate
+    name: String, // this field is 'title' in the amazonAPI
     brand: String,
     image: String,
     price: String,
     rating: Number,
     description: String,
     link: String,
+    bookmarks: Number,
   },
   { collection: "products" }
 );
